dae41f7dc98c40b87b9c1cbb5dafcfe20dea5936
Gor <7495500@gmail.com>
2023-02-21 14:21:47 +0300

    Add download and preview actions

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem "jbuilder"
 gem "redis", "~> 4.0"
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 gem "bootsnap", require: false
+gem "prawn", "~> 2.4"
 
 # gem "kredis"
 # gem "bcrypt", "~> 3.1.7"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "pages#home"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "download", to: 'pages#download'
+  get "preview", to: 'pages#preview'
 end
